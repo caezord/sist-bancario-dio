c3b71ab021bb8bb5e0f64c6eb1ce867dd389bb58
Caê <mcmlins17@gmail.com>
2025-02-15 18:33:01 -0300

    hot fix: saldo var global

--- banco.py
+++ banco.py
@@ -1,26 +1,39 @@
 # 3 funções - extrato, saque e depósito
 import time
 
+saldo = 0
+
 def saque():
-    saldo = 0
+    global saldo
     s = float(input("digite o valor a ser sacado do seu saldo:\n>>> "))
     time.sleep(1.5)
     if(s>saldo):
         print("você não possui saldo o suficiente")
     else:
-        print(f"Seu novo saldo eh de: R${s-saldo}")
+        saldo -= s
+        print(f"Seu novo saldo eh de: R${saldo:.2f}")
 
 def deposito():
-    saldo = 0
+    global saldo
     d = float(input("digite o valor a ser depositado na sua conta:\n>>> "))
     time.sleep(1.5)
-    print(f"Seu novo saldo eh de: R${d+saldo}")
+    saldo += d
+    print(f"Seu novo saldo eh de: R${saldo:.2f}")
+
+def extrato():
+    saldo = 0
 
 nome = input("digite o nome do proprietario da conta:\n>>> ")
+time.sleep(1.5)
 
-op = int(input(f"Bem vindo(a) {nome}, qual sera a operaçao realizada?\n1-saque\n2-deposito\n3-extrato\n>>> "))
+while(1):
+    op = int(input(f"Bem vindo(a) {nome}, qual sera a operaçao realizada?\n1-saque\n2-deposito\n3-extrato\n>>> "))
 
-if(op == 1):
-    saque()
-elif(op==2):
-    deposito()
+    if(op == 1):
+        saque()
+    elif(op==2):
+        deposito()
+    elif(op==3):
+        extrato()
+    else:
+        print("opção inválida")
